97c3434e5f089da8c8493a06df57ce10656256e1
hamdi2050 <hamdi2050@live.com>
2022-12-15 16:55:43 +0100

    Add default tasks

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,15 @@ enum scopeType {
   Completed,
 }
 
+const defaultTasks: TodoProps[] = [
+  { id: "1", completed: true, text: "Complete online JavaScript course" },
+  { id: "2", completed: false, text: "Jog around the park 3x" },
+  { id: "3", completed: false, text: "10 minutes meditation" },
+  { id: "4", completed: false, text: "Read for 1 hour" },
+  { id: "4", completed: false, text: "Pick up groceries" },
+  { id: "5", completed: false, text: "Complete Todo App on Frontend Mentor" },
+];
+
 function App() {
   const [darkMode, setDarkMode] = React.useState(true);
 
@@ -46,10 +55,7 @@ function App() {
   };
 
   React.useEffect(() => {
-    const preferenceQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    if (preferenceQuery.matches) {
-      addDarkMode();
-    }
+    defaultTasks.forEach((task) => addTodoItem(task));
   }, []);
 
   function onClickThemeButton() {
