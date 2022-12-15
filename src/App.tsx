d8097f11fc2a0187afc9d00721203ce1f04239c6
hamdi2050 <hamdi2050@live.com>
2022-12-15 17:04:07 +0100

    Fix default item IDs

--- src/App.tsx
+++ src/App.tsx
@@ -35,8 +35,8 @@ const defaultTasks: TodoProps[] = [
   { id: "2", completed: false, text: "Jog around the park 3x" },
   { id: "3", completed: false, text: "10 minutes meditation" },
   { id: "4", completed: false, text: "Read for 1 hour" },
-  { id: "4", completed: false, text: "Pick up groceries" },
-  { id: "5", completed: false, text: "Complete Todo App on Frontend Mentor" },
+  { id: "5", completed: false, text: "Pick up groceries" },
+  { id: "6", completed: false, text: "Complete Todo App on Frontend Mentor" },
 ];
 
 function App() {
